be1d8efc889be146cb1f72cf0f8e4d7b2970890c
Bismark0B <bismark.ofosu-bamfo@uenr.edu.gh>
2025-11-14 10:12:39 +0000

    penguin plot

---
+++ penguin_plot.R
@@ -0,0 +1,13 @@
+#penguin plot
+library(tidyverse)
+library(palmerpenguins)
+library(ggplot2)
+
+View(penguins)
+
+penguins |> 
+  filter(species == "Adelie") |> 
+  ggplot(aes(x = body_mass_g, y = bill_length_mm)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "Body mass g", y = "Bill length mm", title = "Gentoo")
