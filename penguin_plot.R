e8ded33e623e9487571a1e19d7d7ff6dbf5df645
Bismark0B <bismark.ofosu-bamfo@uenr.edu.gh>
2025-11-14 10:54:17 +0000

    removed "geom_smooth(method = "lm") +"

--- penguin_plot.R
+++ penguin_plot.R
@@ -9,5 +9,4 @@ penguins |>
   filter(species == "Gentoo") |> 
   ggplot(aes(x = body_mass_g, y = bill_length_mm)) +
   geom_point() +
-  geom_smooth(method = "lm") +
   labs(x = "Body mass g", y = "Bill length mm", title = "Adelie")
